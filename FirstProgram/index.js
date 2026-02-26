f91d7fdef0f84f545647d70aa738a507b323950f
Hachibur Rahman <hachiburrahman15@gmail.com>
2026-02-27 01:54:19 +0600

    first typescript code

---
+++ FirstProgram/index.js
@@ -0,0 +1,10 @@
+var userName = "Hachibur Rahman";
+console.log(userName);
+function add(num1, num2) {
+    console.log(num1 + num2);
+}
+function subtract(num1, num2) {
+    console.log(num1 - num2);
+}
+add(20, 10);
+subtract(20, 10);
---
+++ FirstProgram/index.ts
@@ -0,0 +1,11 @@
+let userName = "Hachibur Rahman";
+console.log(userName);
+
+function add(num1: number, num2: number) {
+    console.log(num1 + num2);
+}
+function subtract(num1: number, num2: number) {
+    console.log(num1 - num2);
+}
+add(20, 10);
+subtract(20,10);
