315c28859822d45815591ceceb29fe9f5c2fc115
Hachibur Rahman <hachiburrahman15@gmail.com>
2026-03-06 00:49:12 +0600

    Add array type

---
+++ User-Defined-Type/Array.js
@@ -0,0 +1,12 @@
+var userNames = ["Hachib", "Rahman", "Antu"];
+console.log(userNames);
+for (var _i = 0, userNames_1 = userNames; _i < userNames_1.length; _i++) {
+    var userName = userNames_1[_i];
+    console.log(userName);
+}
+var friends;
+friends = ["Ritu", "Avik", "Soyeb"];
+console.log(friends);
+var ids;
+ids = ["101", "102", "103", 104];
+console.log(ids);
---
+++ User-Defined-Type/Array.ts
@@ -0,0 +1,13 @@
+const userNames:string[] =["Hachib","Rahman","Antu"];
+console.log(userNames);
+
+for(let userName of userNames){
+    console.log(userName);
+
+}
+let friends:string[];
+friends=["Ritu","Avik","Soyeb"];
+console.log(friends);
+let ids:(string | number) [];
+ids=["101","102","103",104];
+console.log(ids);
