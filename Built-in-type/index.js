359c35337c7b2366b6beee595506e523320d120d
Hachibur Rahman <hachiburrahman15@gmail.com>
2026-03-05 16:55:02 +0600

    add Built-In-Type

---
+++ Built-in-type/index.js
@@ -0,0 +1,13 @@
+var userId;
+var firstName;
+var lastName;
+var isActivated;
+userId = 101;
+firstName = "Hachibur";
+lastName = "Rahman";
+isActivated = true;
+console.log(firstName, lastName, userId, isActivated);
+function display() {
+    console.log("Hi,I am Display");
+}
+display();
---
+++ Built-in-type/index.ts
@@ -0,0 +1,15 @@
+let userId:number;
+let firstName:string;
+let lastName:string;
+let isActivated:boolean;
+userId=101;
+firstName="Hachibur";
+lastName="Rahman";
+isActivated=true;
+console.log(firstName,lastName,userId,isActivated);
+
+function display():void{
+    console.log("Hi,I am Display"); 
+}
+
+display();
